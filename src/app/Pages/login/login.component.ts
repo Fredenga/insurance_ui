64749acc05ac1bbd99a501e8873a140973b10a4a
Fredrick Denga <dengafred@gmail.com>
2025-01-14 06:10:07 -0800

    style login page

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  Email: string = '';
+  Password: string = '';
+  onSubmit() {
+    if (this.Email && this.Password) {
+      console.log('Email:', this.Email);
+      console.log('Password:', this.Password);
+      // Here you can add the logic to authenticate the user
+    } else {
+      console.log('Form is invalid!');
+    }
+  }
 }
